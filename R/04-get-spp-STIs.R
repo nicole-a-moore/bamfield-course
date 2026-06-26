5f5d02dba2185be5a934af707d1c610b6bfa517b
jakelawlor <jakealawlor@gmail.com>
2026-06-26 15:29:04 -0400

    get temp through 2025

--- R/04-get-spp-STIs.R
+++ R/04-get-spp-STIs.R
@@ -181,56 +181,9 @@ spp_stis_join$survey_name %in% spp_clean %>% table()
 spp_clean %in% spp_stis_join$survey_name %>% table()
 
 
-# plot
-spp_stis_join %>%
-  mutate(survey_name = forcats::fct_reorder(survey_name, mean_sst)) %>%
-  ggplot(aes(x = survey_name, 
-             y = mean_sst)) +
-  geom_segment(aes(xend = survey_name,
-                   y = q05_sst,
-                   yend = q95_sst)) +
-  geom_point() +
-  theme(axis.text.x = element_text(angle = 45,
-                                   hjust = 1, 
-                                   vjust = 1),
-        plot.margin = margin(l = 40,10,10,10)) +
-  labs(x = "Species",
-       y = "Thermal Index",
-       title = "Wizard Island Thermal Indices") +
-  scale_y_continuous(labels = ~paste0(., "°"))
-
-
 # save csv
 readr::write_csv(spp_stis_join,
                  file = "data-processed/species-thermal-indices/spp-stis.csv")
 
 
 
-
-# get wizard temp ---------------------------------------------------------
-temp <- readr::read_csv("data-processed/env-data/BarkleySound_monthly-sst_1deg-resolution.csv")
-
-yearly_temp <- temp %>%
-  group_by(year) %>%
-  summarize(mean_sst = mean(sst),
-            max_sst = max(sst),
-            min_sst = min(sst))
-
-p_temp <- yearly_temp %>% 
-  tidyr::pivot_longer(cols = c(mean_sst, max_sst, min_sst),
-                      names_to = "var",
-                      values_to = "sst") %>%
-  ggplot(aes(x = year, y = sst, color = var)) +
-  geom_point() +
-  geom_line() +
-  geom_smooth(method = "lm", linetype = "dashed", show.legend = F, alpha = .25) +
-  scale_color_manual(values = c("tomato2", "grey40","cornflowerblue")) + 
-  labs(title = "Barkley Sound SST",
-       x = "Year",
-       y = "SST") +
-  scale_y_continuous(labels = ~paste0(., "°C"))
-  
-
-temp_mod <- lm(data = yearly_temp,
-               formula = "m_sst ~ year")
-summary(temp_mod)
